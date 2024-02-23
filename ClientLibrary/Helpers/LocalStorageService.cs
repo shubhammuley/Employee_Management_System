451c4567b6f70d7e8184181e335f1ba69e3c5a4c
shubhammuley <muleyshubham20@gmail.com>
2024-02-23 19:30:31 +0530

    Create service to consume API in client

---
+++ ClientLibrary/Helpers/LocalStorageService.cs
@@ -0,0 +1,18 @@
+﻿
+using Blazored.LocalStorage;
+
+namespace ClientLibrary.Helpers
+{
+    public class LocalStorageService(ILocalStorageService localStorageService)
+    {
+        private const string storageKey = "authentication-token";
+
+        public async Task<string> GetToken() => await localStorageService.GetItemAsStringAsync(storageKey);
+
+        public async Task SetToken(string item) => await localStorageService.SetItemAsStringAsync(storageKey, item);
+
+        public async Task RemoveToken() => await localStorageService.RemoveItemAsync(storageKey);
+
+
+    }
+}
