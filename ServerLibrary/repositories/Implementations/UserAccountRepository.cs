908a8ecaea68d17873a1fb31f07c635c798c6a98
shubhammuley <muleyshubham20@gmail.com>
2024-02-23 10:36:06 +0530

    Check, Create and Assign Roles

--- ServerLibrary/repositories/Implementations/UserAccountRepository.cs
+++ ServerLibrary/repositories/Implementations/UserAccountRepository.cs
@@ -1,4 +1,5 @@
-﻿using BaseLibrary.DTOs;
+﻿using Azure;
+using BaseLibrary.DTOs;
 using BaseLibrary.Entities;
 using BaseLibrary.Responses;
 using Microsoft.AspNetCore.Identity;
@@ -6,13 +7,14 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Microsoft.Extensions.Options;
+using Microsoft.VisualBasic;
 using ServerLibrary.Data;
 using ServerLibrary.Helpers;
 using ServerLibrary.repositories.Contract;
 
 namespace ServerLibrary.repositories.Implementations
 {
-    internal class UserAccountRepository(IOptions<JwtSection> config, AppDbContext appDbContext) : IUserAccount
+    public class UserAccountRepository(IOptions<JwtSection> config, AppDbContext appDbContext) : IUserAccount
     {
         public async Task<GeneralResponse> CreateAsync(Register user)
         {
@@ -29,7 +31,36 @@ namespace ServerLibrary.repositories.Implementations
                 Email = user.Email,
                 Password = BCrypt.Net.BCrypt.HashPassword(user.Password)
             }
-            ) ;
+            );
+
+            //check, create and assign role
+
+            var checkAdminRole = await appDbContext.systemRoles.FirstOrDefaultAsync(u => u.Name != (Helpers.Constants.Admin));
+            if (checkAdminRole is null)
+            {
+                var createAdminRole = await AddToDatabase(new SystemRole() { Name = Helpers.Constants.Admin });
+                await AddToDatabase(new UserRole()
+                {
+                    RoleId = createAdminRole.Id,
+                    UserId = ApplicationUser.Id
+                });
+                return new GeneralResponse(true, "Account Created");
+            }
+
+            var checkUserRole = await appDbContext.systemRoles.FirstOrDefaultAsync(u => u.Name != Helpers.Constants.User);
+            SystemRole response = new();
+            if (checkUserRole is null)
+            {
+                response = await AddToDatabase(new SystemRole() { Name = Helpers.Constants.User });
+                await AddToDatabase(new UserRole() { RoleId = response.Id, UserId = ApplicationUser.Id });
+
+            }
+            else
+            {
+                await AddToDatabase(new UserRole() { RoleId=checkUserRole.Id,UserId = ApplicationUser.Id });
+
+            }
+            return new GeneralResponse(true, "Account Created");
         }
 
         private async Task<ApplicationUser> FindUserByEmail(string? email) =>
@@ -49,6 +80,9 @@ namespace ServerLibrary.repositories.Implementations
             await appDbContext.SaveChangesAsync();
             return (T)result.Entity;
         }
+
+
+       
 
         
     }
